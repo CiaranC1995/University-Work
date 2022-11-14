b3fba94ff95dd67120f75abe81fd4b6b11ddc9dc
Ciarán Casey <103444158+CiaranC1995@users.noreply.github.com>
2022-11-14 12:50:42 +0000

    Update CSC7061-LottoChecker.java
    Added arrays to store the winning numbers and user choices and also added for loops to reduce the amount of repeated code.

--- CSC7061-LottoChecker.java
+++ CSC7061-LottoChecker.java
@@ -4,6 +4,8 @@
 package lotto;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 
 /**
@@ -13,15 +15,18 @@ import java.util.Random;
  * number is a winner or not. A win counter counts the number of winning balls
  * and outputs a response based on this.
  * 
- * Next step is to split the program into functional methods.
+ * Includes a try catch exception handler for InputMismatchExceptions.
  * 
- * @author ccase
+ * 
+ * @author CiaranC
  *
  */
 public class LottoChecker {
 
 	// Highest Ball number allowed
 	public static final int MAX_NUMBER = 38;
+
+	public static final int MAX_NUMBER_OF_BALLS_IN_DRAW = 6;
 
 	/**
 	 * This method stores a user's favourite 6 lottery numbers along with a record
@@ -30,167 +35,120 @@ public class LottoChecker {
 	 * 
 	 * @param args
 	 */
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		// declare variables for userNumbers and winningNumbers
 
-		int userNum1, userNum2, userNum3, userNum4, userNum5, userNum6;
-		int winner1, winner2, winner3, winner4, winner5, winner6, winCount;
+		int[] userNums = new int[MAX_NUMBER_OF_BALLS_IN_DRAW];
+		int[] winningNums = new int[MAX_NUMBER_OF_BALLS_IN_DRAW];
+		int winCount = 0;
+		boolean complete = false;
 
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 
-		winner1 = random.nextInt(MAX_NUMBER) + 1;
-		winner2 = random.nextInt(MAX_NUMBER) + 1;
-		winner3 = random.nextInt(MAX_NUMBER) + 1;
-		winner4 = random.nextInt(MAX_NUMBER) + 1;
-		winner5 = random.nextInt(MAX_NUMBER) + 1;
-		winner6 = random.nextInt(MAX_NUMBER) + 1;
-
-		// print to screen the player's numbers and the winning numbers
-		System.out.println("Lotto Checker ......");
-
-		// user input for selection of balls
-
-		System.out.println("Select your six numbers (1-38)");
-
-		System.out.println("Enter selection #1:");
-		userNum1 = scanner.nextInt();
-		while (userNum1 <= 0 || userNum1 > MAX_NUMBER) {
-			System.out.println("Please input a valid ball number:");
-			userNum1 = scanner.nextInt();
+		// dynamically populating winning number integer array
+		for (int loop = 0; loop < winningNums.length; loop++) {
+			winningNums[loop] = random.nextInt(MAX_NUMBER) + 1;
 		}
 
-		System.out.println("Enter selection #2:");
-		userNum2 = scanner.nextInt();
-		while (userNum2 <= 0 || userNum2 > MAX_NUMBER) {
-			System.out.println("Please input a valid ball number:");
-			userNum2 = scanner.nextInt();
+		// sorting winning numbers, better for UX
+		Arrays.sort(winningNums);
+
+		do {
+			try {
+				// print to screen the player's numbers and the winning numbers
+				System.out.println("Lotto Checker ......");
+
+				// user input for selection of balls
+
+				System.out.println("Select your six numbers (1-" + MAX_NUMBER + ")");
+
+				for (int loop = 0; loop < userNums.length; loop++) {
+					System.out.println("Enter Selection #" + (loop + 1) + ":");
+					userNums[loop] = scanner.nextInt();
+
+					while (userNums[loop] <= 0 || userNums[loop] > MAX_NUMBER) {
+						System.out.println("Please input a valid ball number:");
+						userNums[loop] = scanner.nextInt();
+					}
+				}
+
+				Arrays.sort(userNums);
+
+				System.out.print("Your selection ... ");
+				printIntegerArray(userNums);
+
+				System.out.print("Winning balls ");
+				printIntegerArray(winningNums);
+
+				// checking for winning numbers - doing this by checking each user number
+				// against the winning numbers
+
+				System.out.println("Checking for winners ...");
+
+				// Nested for loops to check if there are any winning numbers
+				for (int userSelection : userNums) {
+					for (int winner : winningNums) {
+						if (userSelection == winner) {
+							winCount++;
+							System.out.println("A winner  : " + userSelection);
+							break;
+						}
+					}
+				}
+
+				System.out.println("\nOverall result");
+
+				switch (winCount) {
+				case 0:
+					System.out.println("Number of winning balls: " + winCount + " No luck at all!");
+					break;
+				case 1:
+				case 2:
+					System.out.println("Number of winning balls: " + winCount + " Not bad");
+					break;
+				case 3:
+					System.out.println("Number of winning balls: " + winCount + " Good, maybe a trip to the shops");
+					break;
+				case 4:
+					System.out.println(
+							"Number of winning balls: " + winCount + " Don't give up your job but maybe a holiday");
+					break;
+				case 5:
+					System.out.println("Number of winning balls: " + winCount + " Pay your uni fees");
+					break;
+				case 6:
+					System.out.println("Number of winning balls: " + winCount + " Hand in the notice .....");
+					break;
+				default:
+					System.out.println("Error");
+					break;
+				}
+				complete = true;
+				scanner.close();
+
+			} catch (InputMismatchException inputMismatchException) {
+				System.out.println("Incorrect Input - Please try that again");
+				scanner.next();
+				complete = false;
+			}
+		} while (!complete);
+
+	} // end of main
+
+	/**
+	 * Prints to screen the elements of any integer array.
+	 * 
+	 * @param inputIntegerArray - input integer array
+	 */
+	public static void printIntegerArray(int[] inputIntegerArray) {
+
+		for (int loop = 0; loop < inputIntegerArray.length; loop++) {
+			System.out.print(inputIntegerArray[loop] + " ");
 		}
-
-		System.out.println("Enter selection #3:");
-		userNum3 = scanner.nextInt();
-		while (userNum3 <= 0 || userNum3 > MAX_NUMBER) {
-			System.out.println("Please input a valid ball number:");
-			userNum3 = scanner.nextInt();
-		}
-
-		System.out.println("Enter selection #4:");
-		userNum4 = scanner.nextInt();
-		while (userNum4 <= 0 || userNum4 > MAX_NUMBER) {
-			System.out.println("Please input a valid ball number:");
-			userNum4 = scanner.nextInt();
-		}
-
-		System.out.println("Enter selection #5:");
-		userNum5 = scanner.nextInt();
-		while (userNum5 <= 0 || userNum5 > MAX_NUMBER) {
-			System.out.println("Please input a valid ball number:");
-			userNum5 = scanner.nextInt();
-		}
-
-		System.out.println("Enter selection #6:");
-		userNum6 = scanner.nextInt();
-		while (userNum6 <= 0 || userNum6 > MAX_NUMBER) {
-			System.out.println("Please input a valid ball number:");
-			userNum6 = scanner.nextInt();
-		}
-
-		System.out.println("Your selection ... " + userNum1 + ", " + userNum2 + ", " + userNum3 + ", " + userNum4 + ", "
-				+ userNum5 + ", " + userNum6);
-		System.out.println("Winning balls " + winner1 + ", " + winner2 + ", " + winner3 + ", " + winner4 + ", "
-				+ winner5 + ", " + winner6);
-
-		/**
-		 * Alternate method of printing output System.out.printf("Your selections are
-		 * ... %s, %s, %s, %s, %s,
-		 * %s\n",userNum1,userNum2,userNum3,userNum4,userNum5,userNum6);
-		 * System.out.printf("The winning balls are ... %s, %s, %s, %s, %s,
-		 * %s",winner1,winner2,winner3,winner4,winner5,winner6);
-		 */
-
-		// checking for winning numbers - doing this by checking each user number
-		// against the winning numbers
-
-		winCount = 0;
-
-		System.out.println("Checking for winners ...");
-
-		if (userNum1 == winner1 || userNum1 == winner2 || userNum1 == winner3 || userNum1 == winner4
-				|| userNum1 == winner5 || userNum1 == winner6) {
-			System.out.println("Winner " + userNum1);
-			winCount++;
-		} else {
-			System.out.println(userNum1 + " not a winner!");
-		}
-
-		if (userNum2 == winner1 || userNum2 == winner2 || userNum2 == winner3 || userNum2 == winner4
-				|| userNum2 == winner5 || userNum2 == winner6) {
-			System.out.println("Winner " + userNum2);
-			winCount++;
-		} else {
-			System.out.println(userNum2 + " not a winner!");
-		}
-
-		if (userNum3 == winner1 || userNum3 == winner2 || userNum3 == winner3 || userNum3 == winner4
-				|| userNum3 == winner5 || userNum3 == winner6) {
-			System.out.println("Winner " + userNum3);
-			winCount++;
-		} else {
-			System.out.println(userNum3 + " not a winner!");
-		}
-
-		if (userNum4 == winner1 || userNum4 == winner2 || userNum4 == winner3 || userNum4 == winner4
-				|| userNum4 == winner5 || userNum4 == winner6) {
-			System.out.println("Winner " + userNum4);
-			winCount++;
-		} else {
-			System.out.println(userNum4 + " not a winner!");
-		}
-
-		if (userNum5 == winner1 || userNum5 == winner2 || userNum5 == winner3 || userNum5 == winner4
-				|| userNum5 == winner5 || userNum5 == winner6) {
-			System.out.println("Winner " + userNum5);
-			winCount++;
-		} else {
-			System.out.println(userNum5 + " not a winner!");
-		}
-
-		if (userNum6 == winner1 || userNum6 == winner2 || userNum6 == winner3 || userNum6 == winner4
-				|| userNum6 == winner5 || userNum6 == winner6) {
-			System.out.println("Winner " + userNum6);
-			winCount++;
-		} else {
-			System.out.println(userNum6 + " not a winner!");
-		}
-
-		System.out.println("\nOverall result");
-
-		switch (winCount) {
-		case 0:
-			System.out.println("Number of winning balls: " + winCount + " No luck at all!");
-			break;
-		case 1:
-		case 2:
-			System.out.println("Number of winning balls: " + winCount + " Not bad");
-			break;
-		case 3:
-			System.out.println("Number of winning balls: " + winCount + " Good, maybe a trip to the shops");
-			break;
-		case 4:
-			System.out.println("Number of winning balls: " + winCount + " Don't give up your job but maybe a holiday");
-			break;
-		case 5:
-			System.out.println("Number of winning balls: " + winCount + " Pay your uni fees");
-			break;
-		case 6:
-			System.out.println("Number of winning balls: " + winCount + " Hand in the notice .....");
-			break;
-		default:
-			System.out.println("Error");
-			break;
-		}
-		scanner.close();
+		System.out.println();
 	}
 
-}
+} // end of class
